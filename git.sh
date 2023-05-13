9f1a52802e3a166e0d17e33a5a652ac7d4fa3a66
Nya-WSL-JP <server-jp@nya-wsl.com>
2023-05-14 00:47:58 +0900

    change

--- git.sh
+++ git.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cp -r /opt/hexo/* /opt/Nya-WSL-Web
-#cp -r /opt/hexo/source /opt/Nya-WSL-Web-Private
-cp -r /opt/hexo/* /opt/Nya-WSL-CN-Private
-cp -r /etc/nginx/sites-available/default /opt/Nya-WSL-CN-Private/nginx_conf
+cp -r /opt/hexo/* /opt/Nya-WSL-Web-Private
+cp -r /etc/nginx/sites-available/default /opt/Nya-WSL-Web-Private/nginx_conf
 
 function git_command {
 git add .
@@ -12,5 +11,5 @@ git push
 
 cd /opt/Nya-WSL-Web
 git_command
-cd /opt/Nya-WSL-CN-Private
+cd /opt/Nya-WSL-Web-Private
 git_command
